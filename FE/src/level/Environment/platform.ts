520f96c4145efd0f6f6a45a90ccdbaa8255b948f
Ehud Marchi <Ehud.Marchi@amdocs.com>
2022-05-15 16:03:25 +0300

    isStandingFloor

--- FE/src/level/Environment/platform.ts
+++ FE/src/level/Environment/platform.ts
@@ -3,9 +3,10 @@ import { makeSprite, t } from "@replay/core";
 export type PlatformT = {
   x: number;
   y: number;
+  isWide :boolean
 };
 
-type PlatformProps = {
+export type PlatformProps = {
   platform: PlatformT;
 };
 
@@ -14,8 +15,8 @@ export const Platform = makeSprite<PlatformProps>({
     const { platform } = props;
     return [
       t.image({
-        height: 12, width: 16,
-        fileName: "platform.png",
+        height:12, width: platform.isWide?48:16,
+        fileName: platform.isWide? "wide_platform.png": "platform.png",
         x: platform.x,
         y: platform.y
       }),
